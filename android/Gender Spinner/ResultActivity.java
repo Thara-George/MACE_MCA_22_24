56200bd551f5a9cc152901ea480ef0f32a12dca3
Thara-George <117899588+Thara-George@users.noreply.github.com>
2023-12-18 11:12:21 +0530

    Update ResultActivity.java

--- android/Gender Spinner/ResultActivity.java
+++ android/Gender Spinner/ResultActivity.java
@@ -1,1 +1,27 @@
+package com.example.girlboy;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        // Get the selected gender from the intent
+        String selectedGender = getIntent().getStringExtra("SELECTED_GENDER");
+
+        // Set the image based on the selected gender
+        ImageView genderImageView = findViewById(R.id.genderImageView);
+        if ("Male".equals(selectedGender)) {
+            genderImageView.setImageResource(R.drawable.male); // Replace with your male image resource
+        } else if ("Female".equals(selectedGender)) {
+            genderImageView.setImageResource(R.drawable.female); // Replace with your female image resource
+        }
+    }
+}
 
